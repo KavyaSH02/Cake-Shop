b3067116b9b90c999d0e84cc4b485b1826fde981
KavyaSH02 <kavyahiremath.mindsnxt@gmail.com>
2026-01-13 12:12:02 +0530

    Updates

---
+++ _next/static/3XusI8TP2togDsf0K9kD9/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Cake-Shop//_next/:path+",
+        "destination": "/Cake-Shop/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
